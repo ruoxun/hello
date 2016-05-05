c917edc9cde650422c333c0f9a413b453850a4f0
unknown <unknown@unknown.com>
2016-05-05 17:54:31 +0800

    main.py

---
+++ main.py
@@ -0,0 +1,39 @@
+class Ellipse():
+	def _init_(self,a,b,):
+		self.a = a
+		self.b = b
+		def _init_(self,a,b):
+			def getA(self):
+				return self.a
+			def getB(self):
+				return self.b
+				long.a = Ellipse('20')
+				short.b = Ellipse('10')
+			def get_area(self):
+				return Ellipse.pi*self.a*self.b
+			print(Ellipse.get_area(20,10))
+			class Square(Ellipse):
+				Ellipse._init_(self,a)
+				a = Square('15')
+				def get_area(self):
+					return Square.pi*self.a*self.a
+				print(Square.get_area(15))
+				class Rectangle(Ellipse):
+					Ellipse._init_(self,a,b)
+					a = Rectangle('20')
+					b = Rectangle('15')
+					def get_area(self):
+						return Rectangle.self.a*self.b
+			print(Rectangle.get_area(20,15))
+			class circle(Ellipse):
+				Ellipse._init_(self,a)
+				a = Circle('5')
+				def get_area(self):
+					return Circle.pi*self.a*self.a
+				print(Square.get_area(5))
+
+	def compute_area(shapes):
+		shapes = [Ellipse(10,20),Square(15),Circle(5),Rectangle(20,15),Circle(5),Square(15),Ellipse(10,20)]
+		total_area = compute_area(shapes)
+		print(total_area)
+
